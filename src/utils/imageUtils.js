3852a13640eb36c111e6f29424cf07601f2d19c0
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-14 19:48:26 +0100

    Update imageUtils.js

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -1,10 +1,1 @@
-export const loadImageMetadata = async (src) => {
-  try {
-    const response = await fetch(`/assets/images/metadata/${src.replace(/\.[^/.]+$/, "")}.json`);
-    if (!response.ok) throw new Error('Failed to load image metadata');
-    return await response.json();
-  } catch (error) {
-    console.error('Error loading image metadata:', error);
-    throw error;
-  }
-};
+placeholder
